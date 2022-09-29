9a157d11c47295b8c466004fd94d3ea61609cc90
Sergio <sergioruza22@gmail.com>
2022-09-29 16:06:57 -0300

    Requisito um finalizado

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,5 +5,7 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
-
+if (window.Cypress) {
+  window.store = store;
+}
 export default store;
